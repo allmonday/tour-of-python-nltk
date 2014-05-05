1694f9b5960b523d3774989b64b48d44c3baf85f
Tangkikodo, Tang Min <allmonday@126.com>
2014-05-05 16:32:11 +0800

    init

---
+++ ch08_struct_of_sentence/pajamas.py
@@ -0,0 +1,17 @@
+import nltk
+groucho_grammar = nltk.parse_cfg("""
+    s -> NP VP
+    PP -> P NP
+    NP -> Det N | Det N PP | 'I'
+    VP -> V NP | VP PP
+    Det -> 'an' | 'my'
+    N -> 'elephant' | 'pajamas'
+    V -> 'shot'
+    P -> 'in'                        
+    """ 
+)
+sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
+parser = nltk.ChartParser(groucho_grammar)
+trees = parser.nbest_parse(sent)
+for tree in trees:
+    print tree
